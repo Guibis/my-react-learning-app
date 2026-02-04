de3bf2560a5d75b906c3f405eb21a90b3c4943fe
Guilherme Pereira Lima <105331166+Guibis@users.noreply.github.com>
2026-02-04 18:46:40 -0300

    React task3 (#1)
    * feat: Implement initial React App component with mood state, interactive buttons, and dynamic background color.
    
    * feat: implement initial app component to fetch and display random user data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import MyButton from './MyButton'
-import UserAvatar from './UserAvatar'
-import StatusBadge from './StatusBadge'
+import { useState, useEffect } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/")
+      .then(response => response.json())
+      .then(data => setUserData(data));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <div className="cardUsers">
-          <div className="cardUser">
-            <MyButton label="Katherine Johnson" color="red" />
-            <UserAvatar ImageURL="https://i.imgur.com/MK3eW3As.jpg" size={100} />
-            <StatusBadge status="offline" />
-          </div>
-          <div className="cardUser">
-            <MyButton label="Alan L. Hart" color="green" />
-            <UserAvatar ImageURL="https://i.imgur.com/QIrZWGIs.jpg" size={100} />
-            <StatusBadge status="online" />
-          </div>
-          <div className="cardUser">
-            <MyButton label="Hedy Lamarr" color="orange" />
-            <UserAvatar ImageURL="https://i.imgur.com/yXOvdOSs.jpg" size={100} />
-            <StatusBadge status="away" />
-          </div>
-        </div>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {userData && (
+        <body style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", textAlign: "center"}}>
+          <h1>{userData.results[0].name.first} {userData.results[0].name.last}</h1>
+          <p>{userData.results[0].email}</p>
+          <img src={userData.results[0].picture.large} alt="" />
+        </body>
+      )}
     </>
-  )
+  );
 }
-
-export default App
